2e9360092560513fd890db05545e2c2c58fd1560
PonceDeLeon882 <112857124+PonceDeLeon882@users.noreply.github.com>
2022-11-21 13:17:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function creatBlock() {
+    let box = document.querySelector('.box') 
+    let newBlock = document.createElement('div')
+    newBlock.classList = ('mini')
+    
+    box.append(newBlock)
+    }
+
+
